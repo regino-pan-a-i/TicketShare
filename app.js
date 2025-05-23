366cc3731707c5619f583356c53168a8c64e5eb2
Andre Regino <reginopanai@gmail.com>
2025-05-22 23:22:00 -0600

    initial route works

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-import fastify from "fastify";
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+  logger: true
+})
 
 
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
 
-const init = async() => {
-    try{
-
-        fastify.listen({port:3000})
-        console.log('listening in port 3000')
-    }
-    catch (error) { 
-        console.log('ups, something went wrong')
-    }
+/**
+ * Run the server!
+ */
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
 }
 
-
-init()
+start()
